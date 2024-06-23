591d212a37d54c9bcbba88a7cc015b33bbbc31cc
Somnath <dsomnath627@gmail.com>
2024-06-23 16:25:55 +0530

    Added a header section in index page

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,15 @@ import CryptoTable from "@/components/CryptoTable";
 export default function Home() {
   
   return (
-    <main>
-      <nav>
-
+    <main className="container">
+      <nav className="flex flex-col items-center space-y-4 w-full py-20 bg-blue-100">
+        <h1 className="text-3xl font-medium">Crypto App</h1>
+        <p className="text-sm text-center">Get latest crypto data here.</p>
       </nav>
+      <div className="mt-10 max-w-4xl mx-auto">
+
       <CryptoTable />
+      </div>
     </main>
   );
 }
